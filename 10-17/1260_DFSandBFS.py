e925a178273e3af3ff9d9da175092f43d004ece8
Lee Jonghwa <misterlee95@gmail.com>
2024-10-21 17:50:56 +0900

    1021

--- 10-17/1260_DFSandBFS.py
+++ 10-17/1260_DFSandBFS.py
@@ -10,6 +10,55 @@ M: 간선 개수
 V: 시작점
 """
 
+def dfs(V):
+    s = V
+
+    for w in adjL[s]:
+        if visited[w] == 1: continue
+        path1.append(w)
+        visited[w] = 1
+        dfs(w)
+        visited[w] = 0
+        path1.pop()
+
+    return path1
+
+
+def bfs(V):
+    q = []
+    q.append(V)
+    visited[V] = 1
+    path = [V]
+
+    while q:
+        s = q.pop(0)
+
+        for w in adjL[s]:
+            if visited[w] == 0:
+                visited[w] = 1
+                q.append(w)
+                path.append(w)
+
+    return path
+
+adjL = [[] for _ in range(N+1)]
 Lines = [list(map(int, input().split())) for _ in range(M)]
 
-print(Lines)
+for i in range(M):
+    a = Lines[i][0]
+    b = Lines[i][1]
+    adjL[a].append(b)
+    adjL[b].append(a)
+
+print(adjL)
+
+visited = [0]*(N+1)
+visited[V] = 1
+path1 = [V]
+path1 = dfs(V)
+
+visited = [0]*(N+1)
+path2 = bfs(V)
+
+print(*path1)
+print(*path2)
